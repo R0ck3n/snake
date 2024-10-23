69c7560b86765aa71f48fac024a5c50c462b3c93
Nicolas Gracia <r0kk3nn@gmail.com>
2024-10-23 10:33:18 +0200

    affichage tete snake

--- src/js/board.js
+++ src/js/board.js
@@ -1,1 +1,23 @@
-const gameBoard = document.querySelector('#board-game')
+const canvas = document.querySelector('#board-game')
+const ctx = canvas.getContext("2d");
+
+
+const box = 20;
+
+
+
+const snake = {
+    position : {
+        x : (canvas.width / 2) - box /2 ,
+        y: (canvas.height / 2) - box /2
+    }
+}
+
+/** Dessine un carré sur le canvas */
+function drawSquare (x,y,size, color) {
+
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, size, size);
+}
+
+drawSquare(snake.position.x,snake.position.y, box, 'white');
