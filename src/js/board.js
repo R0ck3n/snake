030959110b88098ec29c3cb7dba1f6a58baee152
Theo <theokimbangui36@gmail.com>
2024-10-23 12:20:29 +0200

    feat: move snake

--- src/js/board.js
+++ src/js/board.js
@@ -8,7 +8,6 @@ const snakeHead = {
 };
 let direction = "right";
 
-
 const snakeHeadImg = new Image();
 snakeHeadImg.src = "../../public/snake_green_head_32.png";
 
@@ -22,81 +21,84 @@ function drawSquare(x, y, size, color) {
   ctx.fillRect(x, y, size, size);
 }
 
-
-
 function updateMove(direction) {
   switch (direction) {
     // haut
-    case 'up':
+    case "up":
       snakeHead.y -= box;
       break;
     // bas
-    case 'down':
-        snakeHead.y += box;
+    case "down":
+      snakeHead.y += box;
       break;
 
     // gauche
-    case 'left':
-        snakeHead.x  -= box
+    case "left":
+      snakeHead.x -= box;
       break;
 
     // droite
-    case 'right':
-      snakeHead.x  += box
+    case "right":
+      snakeHead.x += box;
       break;
   }
 }
 
-
 function updateCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawSquare(snakeHead.x, snakeHead.y, box, "white");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawSquare(snakeHead.x, snakeHead.y, box, "white");
 }
 
-function gameInterval () {
-    const interval = setInterval(()=>{
-        updateCanvas()
-        updateMove(direction)
-    },150)
+function gameInterval() {
+  const interval = setInterval(() => {
+    if (checkCollision()) {
+      clearInterval(interval);
+      alert("Game Over");
+    } else {
+      updateCanvas();
+
+      updateMove(direction);
+    }
+  }, 100);
 }
 
 function changeDirection(ev) {
-    ev.prev
-    const key = ev.keyCode;
-    console.log(key);
-    
-    switch (key) {
-        // haut
-        case 38:
-          direction = 'up'
-          break;
-        // bas
-        case 40:
-            direction ='down'
-          break;
-    
-        // gauche
-        case 37:
-            direction = 'left'
-          break;
-    
-        // droite
-        case 39:
-          direction = 'right'
-          break;
-      }
+  ev.preventDefault();
+  const key = ev.keyCode;
+  switch (key) {
+    // haut
+    case 38:
+      direction = "up";
+      break;
+    // bas
+    case 40:
+      direction = "down";
+      break;
+
+    // gauche
+    case 37:
+      direction = "left";
+      break;
+
+    // droite
+    case 39:
+      direction = "right";
+      break;
+  }
 }
 
-function checkCollision() <<<<<<<<<<<<<<<< 
+function checkCollision() {
+  if (
+    snakeHead.x >= canvas.width ||
+    snakeHead.x < 0 ||
+    snakeHead.y >= canvas.height ||
+    snakeHead.y < 0
+  ) {
+    return true;
+  }
 
-
-
-
-
-
-
-
-
+  return false;
+}
 
 document.addEventListener("keydown", changeDirection);
 
