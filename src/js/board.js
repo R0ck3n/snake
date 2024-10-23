5e0bdb63aec9308e1f8f26a56f72f56b5228b9f1
Nicolas Gracia <r0kk3nn@gmail.com>
2024-10-23 11:30:21 +0200

    deplacement ok en manuel de la tete

--- src/js/board.js
+++ src/js/board.js
@@ -3,17 +3,15 @@ const ctx = canvas.getContext("2d");
 
 const box = 20;
 const snakeHead = {
-  x: (canvas.width - box) / 2 ,
-  y: (canvas.height - box) / 2 
+  x: (canvas.width - box) / 2,
+  y: (canvas.height - box) / 2,
 };
 const snakeHeadImg = new Image();
-snakeHeadImg.src = '../../public/snake_green_head_32.png'
+snakeHeadImg.src = "../../public/snake_green_head_32.png";
 
-
-snakeHeadImg.onload = () => {
-    ctx.drawImage(snakeHeadImg, snakeHead.x, snakeHead.y, box, box);
-  };
-
+//snakeHeadImg.onload = () => {
+//     ctx.drawImage(snakeHeadImg, snakeHead.x, snakeHead.y, box, box);
+//   };
 
 /** Dessine un carré sur le canvas */
 function drawSquare(x, y, size, color) {
@@ -21,6 +19,50 @@ function drawSquare(x, y, size, color) {
   ctx.fillRect(x, y, size, size);
 }
 
-//drawSquare(snakeHead.x, snakeHead.y, box, "white");
 
 
+function move(key) {
+
+
+  switch (key) {
+    // haut
+    case 38:
+      snakeHead.y -= box;
+      break;
+    // bas
+    case 40:
+        snakeHead.y += box;
+      break;
+
+    // gauche
+    case 37:
+        snakeHead.x  -= box
+      break;
+
+    // droite
+    case 39:
+      snakeHead.x  += box
+      break;
+  }
+}
+
+
+function updateCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawSquare(snakeHead.x, snakeHead.y, box, "white");
+}
+
+function gameInterval () {
+    const interval = setInterval(()=>{
+        updateCanvas()
+    },150)
+}
+
+document.addEventListener("keydown", (ev) => {
+  ev.preventDefault();
+  move(ev.keyCode)
+  console.log(ev.keyCode);
+});
+
+updateCanvas();
+gameInterval();
