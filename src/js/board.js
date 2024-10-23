73e952577aad0f8e1314aa19f1683aa9e996d9ea
Nicolas Gracia <r0kk3nn@gmail.com>
2024-10-23 10:36:06 +0200

    micro refacto

--- src/js/board.js
+++ src/js/board.js
@@ -1,23 +1,17 @@
-const canvas = document.querySelector('#board-game')
+const canvas = document.querySelector("#board-game");
 const ctx = canvas.getContext("2d");
-
 
 const box = 20;
 
-
-
-const snake = {
-    position : {
-        x : (canvas.width / 2) - box /2 ,
-        y: (canvas.height / 2) - box /2
-    }
-}
+const snakeHead = {
+  x: (canvas.width - box) / 2 ,
+  y: (canvas.height - box) / 2 
+};
 
 /** Dessine un carré sur le canvas */
-function drawSquare (x,y,size, color) {
-
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, size, size);
+function drawSquare(x, y, size, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, size, size);
 }
 
-drawSquare(snake.position.x,snake.position.y, box, 'white');
+drawSquare(snakeHead.x, snakeHead.y, box, "white");
